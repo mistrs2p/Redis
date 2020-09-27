ee88e365fefa8c76aa3f37c504d8ecef533e04c6
MistRain@P <mahdimousavi40@gmail.com>
2020-09-27 11:50:53 +0330

    database migration-> title added

--- database/migrations/2020_09_27_081748_create_articles_table.php
+++ database/migrations/2020_09_27_081748_create_articles_table.php
@@ -15,6 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->title();
             $table->timestamps();
         });
     }
