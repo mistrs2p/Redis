3c6947e53fcd91cc2392b5a5d1b713c41fd94cbe
MistRain@P <mahdimousavi40@gmail.com>
2020-09-28 17:40:17 +0330

    redis exp set time to expire added

--- routes/web.php
+++ routes/web.php
@@ -14,61 +14,87 @@ use Illuminate\Support\Facades\Redis;
 |
 */
 
+// Route::get('/', function () {
+//     // return view('welcome');
+//     $visits = Redis::incrBy('visits', 5);
+//     return view('welcome')->withVisits($visits);
+// });
+
+// Route::get('videos/{id}', function ($id) {
+//     $downlaods = Redis::get("videos.{$id}.downloads");
+
+//     return view('welcome')->withDownloads($downlaods);
+// });
+
+// Route::get('videos/{id}/download', function ($id) {
+//     // Prepare the download
+//     Redis::incr("videos.{$id}.downloads");
+//     return back();
+// });
+
+
+
+// Route::get('articles/trending', function () {
+//     $trending = Redis::zrevrange('trending_articles', 0, 2);
+
+//     $trending = App\Article::hydrate(
+//         array_map('json_decode', $trending)
+//     );
+
+//     dd($trending);
+//     // return $trending;
+// });
+
+// Route::get('articles/{article}', function (App\Article $article) {
+//    Redis::zincrby('trending_articles', 1, $article); // $article->id same output
+
+//    Redis::zremrangebyrank('trending_article', 0, -4);
+
+//    return $article; 
+// });
+
+// // Hashes
+
+// Route::get('/hashes', function () {
+
+//     $user1stats = [
+//         'favorites' => 50,
+//         'watchLaters' => 95,
+//         'compelitions' => 29
+//     ];
+
+//     Redis::hmset('user.1.stats', $user1stats);
+
+//     return Redis::hgetall('user.1.stats');
+
+// });
+
+// Route::get('users/{id}/stats', function ($id) {
+//     return Redis::hgetall("user.{$id}.stats");
+// });
+
+
+function remember($key, $minuts, $callback) {
+    if ($value = Redis::get($key)) {
+        return json_decode($value);
+    }
+
+    $value = $callback();
+
+    Redis::setex($key, $minuts, $value);
+
+    return $value;
+}
+
 Route::get('/', function () {
-    // return view('welcome');
-    $visits = Redis::incrBy('visits', 5);
-    return view('welcome')->withVisits($visits);
-});
 
-Route::get('videos/{id}', function ($id) {
-    $downlaods = Redis::get("videos.{$id}.downloads");
+    if(Redis::exists('articles.all')) {
+        return json_decode(Redis::get('articles.all'));
+    }
 
-    return view('welcome')->withDownloads($downlaods);
-});
+    $articles = App\Article::all();
 
-Route::get('videos/{id}/download', function ($id) {
-    // Prepare the download
-    Redis::incr("videos.{$id}.downloads");
-    return back();
-});
+    Redis::setex('articles.all', 10, $articles);
 
-
-
-Route::get('articles/trending', function () {
-    $trending = Redis::zrevrange('trending_articles', 0, 2);
-
-    $trending = App\Article::hydrate(
-        array_map('json_decode', $trending)
-    );
-
-    dd($trending);
-    // return $trending;
-});
-
-Route::get('articles/{article}', function (App\Article $article) {
-   Redis::zincrby('trending_articles', 1, $article); // $article->id same output
-
-   Redis::zremrangebyrank('trending_article', 0, -4);
-
-   return $article; 
-});
-
-// Hashes
-
-Route::get('/hashes', function () {
-
-    $user1stats = [
-        'favorites' => 50,
-        'watchLaters' => 95,
-        'compelitions' => 29
-    ];
-
-    Redis::hmset('user.1.stats', $user1stats);
-
-    return Redis::hgetall('user.1.stats');
-
-});
-
-Route::get('users/{id}/stats', function ($id) {
-    return Redis::hgetall("user.{$id}.stats");
+    return $articles;
 });
